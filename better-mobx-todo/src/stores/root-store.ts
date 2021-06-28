224d6f25c4548f2de31d87d16560aca4fc6444de
Joris Jansen <joris97jansen@gmail.com>
2021-06-28 07:03:42 +0200

    Finished React + MobX

--- better-mobx-todo/src/stores/root-store.ts
+++
@@ -1,9 +0,0 @@
-export default class RootStore {
-    dataStore: DataStore;
-    uiStore: UiStore;
-
-    constructor() {
-        this.dataStore = new DataStore(this);
-        this.uiStore = new UiStore(this);
-    }
-}
